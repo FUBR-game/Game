4b959d5e969f3df24ef89d4df67655faf7924c8b
Benjamin <WhenToBen@users.noreply.github.com>
2020-01-21 09:52:15 +0100

    API communicates with the server now
    Also the game will now check up to 5 ports when starting in server mode to bind with.

--- Assets/Scripts/NetworkProvider.cs
+++ Assets/Scripts/NetworkProvider.cs
@@ -2,7 +2,10 @@
 using BeardedManStudios.Forge.Networking.Unity;
 using UnityEngine;
 using System;
+using System.Net.Sockets;
 using UnityEngine.SceneManagement;
+using System.Text;
+using System.Net;
 
 public class NetworkProvider : MonoBehaviour
 {
@@ -10,6 +13,9 @@ public class NetworkProvider : MonoBehaviour
     public bool debug = true;
     public string ip = "127.0.0.1";
     public ushort port = 15937;
+    public string apiIp = "62.210.180.72";
+    public ushort apiPort = 61683;
+
 
     private NetworkManager mgr;
     public GameObject networkManager;
@@ -26,10 +32,29 @@ public class NetworkProvider : MonoBehaviour
 
         if (isServer)
         {
+            var publicIp = new WebClient().DownloadString("http://bot.whatismyipaddress.com");
+
             //Server setup
             NetWorker.PingForFirewall(port);
-
             Host();
+            var client = new System.Net.Sockets.TcpClient();
+            var ipendpoint = new IPEndPoint(IPAddress.Parse(apiIp), apiPort);
+            client.Connect(ipendpoint);
+            var message = new { MessageType = "ServerStarted", MessageData = new { ServerIP = publicIp, ServerPort = port } };
+            var jsonMsg = "{ \"MessageType\": \"ServerStarted\", \"MessageData\": { \"ServerIP\": \"" + publicIp + "\", \"ServerPort\": " + port + "}}";
+            Debug.Log(jsonMsg.ToString());
+            var stream = client.GetStream();
+            var messageByteArray = Encoding.UTF8.GetBytes(jsonMsg);
+            var messageLength = messageByteArray.Length;
+            var messageLengthByteArray = new byte[4];
+            messageLengthByteArray = BitConverter.GetBytes(messageLength);
+            var fullMessage = new byte[messageLength + 4];
+            Buffer.BlockCopy(messageLengthByteArray, 0, fullMessage, 0, messageLengthByteArray.Length);
+            Buffer.BlockCopy(messageByteArray, 0, fullMessage, messageLengthByteArray.Length, messageByteArray.Length);
+            stream.Write(fullMessage, 0, fullMessage.Length);
+            stream.Flush();
+            stream.Close();
+            client.Close();
         }
         else
             Connect();
@@ -43,7 +68,19 @@ public class NetworkProvider : MonoBehaviour
     public void Host()
     {
         server = new UDPServer(maxPlayers);
-        ((UDPServer) server).Connect(ip, port);
+        for(int i = 0; i < 5; i++)
+        {
+            try
+            {
+                ((UDPServer)server).Connect(ip, port);
+                break;
+            }
+            catch(FailedBindingException ignored)
+            {
+                port++;
+                continue;
+            }
+        }
         Connected(server);
     }
 
