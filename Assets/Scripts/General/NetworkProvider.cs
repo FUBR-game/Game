5e3eba68584ed06e54232f9d90b694cf77ff71f0
twan terschegget <twanterschegget@hotmail.nl>
2020-01-22 09:50:25 +0100

    Clienten argument code

--- Assets/Scripts/General/NetworkProvider.cs
+++ Assets/Scripts/General/NetworkProvider.cs
@@ -12,10 +12,10 @@ public class NetworkProvider : MonoBehaviour
     public bool debug = true;
     public string ip = "127.0.0.1";
     public ushort port = 15937;
-    
+
     public string apiIp = "62.210.180.72";
     public ushort apiPort = 61683;
-    
+
     private NetworkManager mgr;
     public GameObject networkManager;
 
@@ -25,40 +25,40 @@ public class NetworkProvider : MonoBehaviour
     private UDPClient client;
 
     private LogHandler _logger;
+    private string publicIp;
 
     // Start is called before the first frame update
     void Start()
     {
         string[] arguments = Environment.GetCommandLineArgs();
+
+        if (arguments.Length != 0)
+        {
+            for (var index = 0; index < arguments.Length; index++)
+            {
+                if (arguments[index] == "-ip")
+                {
+                    ip = arguments[index + 1];
+                }
+                else if (arguments[index] == "-port")
+                {
+                    port = ushort.Parse(arguments[index + 1]);
+                }
+            }
+        }
+
         Rpc.MainThreadRunner = MainThreadManager.Instance;
 
         _logger = LogHandler.Instance;
 
         if (isServer)
         {
-            var publicIp = new WebClient().DownloadString("http://bot.whatismyipaddress.com");
+            publicIp = new WebClient().DownloadString("http://bot.whatismyipaddress.com");
 
             //Server setup
             NetWorker.PingForFirewall(port);
             Host();
-            var client = new System.Net.Sockets.TcpClient();
-            var ipendpoint = new IPEndPoint(IPAddress.Parse(apiIp), apiPort);
-            client.Connect(ipendpoint);
-            var message = new { MessageType = "ServerStarted", MessageData = new { ServerIP = publicIp, ServerPort = port } };
-            var jsonMsg = "{ \"MessageType\": \"ServerStarted\", \"MessageData\": { \"ServerIP\": \"" + publicIp + "\", \"ServerPort\": " + port + "}}";
-            Debug.Log(jsonMsg.ToString());
-            var stream = client.GetStream();
-            var messageByteArray = Encoding.UTF8.GetBytes(jsonMsg);
-            var messageLength = messageByteArray.Length;
-            var messageLengthByteArray = new byte[4];
-            messageLengthByteArray = BitConverter.GetBytes(messageLength);
-            var fullMessage = new byte[messageLength + 4];
-            Buffer.BlockCopy(messageLengthByteArray, 0, fullMessage, 0, messageLengthByteArray.Length);
-            Buffer.BlockCopy(messageByteArray, 0, fullMessage, messageLengthByteArray.Length, messageByteArray.Length);
-            stream.Write(fullMessage, 0, fullMessage.Length);
-            stream.Flush();
-            stream.Close();
-            client.Close();
+            NotifyManager();
         }
         else
             Connect();
@@ -69,21 +69,46 @@ public class NetworkProvider : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
+    private void NotifyManager()
+    {
+        var client = new System.Net.Sockets.TcpClient();
+        var ipendpoint = new IPEndPoint(IPAddress.Parse(apiIp), apiPort);
+        client.Connect(ipendpoint);
+        var message = new
+            {MessageType = "ServerStarted", MessageData = new {ServerIP = publicIp, ServerPort = port}};
+        var jsonMsg = "{ \"MessageType\": \"ServerStarted\", \"MessageData\": { \"ServerIP\": \"" + publicIp +
+                      "\", \"ServerPort\": " + port + "}}";
+        Debug.Log(jsonMsg.ToString());
+        var stream = client.GetStream();
+        var messageByteArray = Encoding.UTF8.GetBytes(jsonMsg);
+        var messageLength = messageByteArray.Length;
+        var messageLengthByteArray = new byte[4];
+        messageLengthByteArray = BitConverter.GetBytes(messageLength);
+        var fullMessage = new byte[messageLength + 4];
+        Buffer.BlockCopy(messageLengthByteArray, 0, fullMessage, 0, messageLengthByteArray.Length);
+        Buffer.BlockCopy(messageByteArray, 0, fullMessage, messageLengthByteArray.Length, messageByteArray.Length);
+        stream.Write(fullMessage, 0, fullMessage.Length);
+        stream.Flush();
+        stream.Close();
+        client.Close();
+    }
+
     public void Host()
     {
         server = new UDPServer(maxPlayers);
-        for(int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++)
         {
             try
             {
-                ((UDPServer)server).Connect(ip, port);
+                ((UDPServer) server).Connect(ip, port);
                 break;
             }
-            catch(FailedBindingException)
+            catch (FailedBindingException)
             {
                 port++;
             }
         }
+
         Connected(server);
     }
 
